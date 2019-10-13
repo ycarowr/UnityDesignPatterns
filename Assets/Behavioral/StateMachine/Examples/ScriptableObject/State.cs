de038455e421ee6eacaaca3a8b4c0dc859dc13ee
ycarowr <ywrycaro@gmail.com>
2019-10-13 07:59:29 +0300

    Add scriptable object state machine with some tests

---
+++ Assets/Behavioral/StateMachine/Examples/ScriptableObject/State.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Tools.Patterns.StateMachineSO
+{
+    public interface IState
+    {
+        void Enter();
+        void Exit();
+        void Update();
+        void Clear();
+    }
+
+    public class State : IState
+    {
+        void IState.Clear() => OnClearState();
+
+        void IState.Enter() => OnEnterState();
+
+        void IState.Exit() => OnExitState();
+
+        void IState.Update() => OnUpdateState();
+
+        protected virtual void OnInitializeState() => Debug.Log("On Initialize: " + GetType());
+        protected virtual void OnEnterState() => Debug.Log("On Enter: " + GetType());
+        protected virtual void OnExitState() => Debug.Log("On Exit: " + GetType());
+
+        protected virtual void OnUpdateState()
+        {
+        }
+
+        protected virtual void OnClearState()
+        {
+        }
+    }
+}
