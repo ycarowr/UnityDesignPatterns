5d95cf3317c93ec238fff654590216574862f018
Ycaro Weschenfelder Rodrigues <ywrycaro@gmail.com>
2021-03-18 17:38:46 +0200

    Add type object structure

---
+++ Assets/GameProgramming/TypeObject/Structure/Structure.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace TypeObject
+{
+    /// <summary>
+    ///     The typed object that contains a reference that represents the type itself.
+    /// </summary>
+    public class TypedObject
+    {
+        private readonly TypeObject _type;
+
+        public TypedObject(TypeObject type)
+        {
+            _type = type;
+        }
+    }
+
+    /// <summary>
+    ///     The Type of the <see cref="TypedObject"/>>.
+    /// </summary>
+    public class TypeObject
+    {
+        public readonly TypeObject Parent;
+
+        public TypeObject(TypeObject parent)
+        {
+            Parent = parent;
+        }
+    }
+
+
+    public class Structure
+    {
+        public Structure()
+        {
+            var type = new TypeObject(null);
+            
+            var typedObject = new TypedObject(type);
+        }
+    }
+}
