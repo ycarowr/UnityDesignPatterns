d5b62b581fe6c2ee3e519fafd05b968b4d62b8a7
Ycaro Weschenfelder Rodrigues <ywrycaro@gmail.com>
2021-03-18 16:14:55 +0200

    Add a better initialization

--- Assets/Creational/ServiceLocator/Scripts/ServiceLocator.cs
+++ Assets/Creational/ServiceLocator/Scripts/ServiceLocator.cs
@@ -13,28 +13,12 @@ namespace ServiceLocatorExample
     public static class ServiceLocator
     {
         private static readonly Dictionary<Type, object> _registry = new Dictionary<Type, object>();
-        public static bool IsInitialized { get; private set; }
-
-        public static void InitializeServices()
-        {
-            var logger = new ServiceLogger();
-
-            //Note: the binding happens with the interface.
-            RegisterService<ILogger>(logger);
-            RegisterService<IAudioPlayer>(new ServiceAudio());
-            RegisterService<IAchievements>(new ServiceAchievements());
-            IsInitialized = true;
-            logger.Log("Services", "Initialized successfully");
-        }
 
         /// <summary>
         ///     Returns null in case the service is not registered or services not initialized.
         /// </summary>
         public static T GetService<T>() where T : class
         {
-            if (!IsInitialized)
-                return null;
-
             var type = typeof(T);
             var containsService = _registry.ContainsKey(type);
             return containsService ? _registry[type] as T : null;
@@ -43,7 +27,7 @@ namespace ServiceLocatorExample
         /// <summary>
         ///     Registers a service in the list.
         /// </summary>
-        private static void RegisterService<T>(IService service) where T : IService
+        public static void RegisterService<T>(IService service) where T : IService
         {
             var type = typeof(T);
             _registry[type] = service;
--- Assets/Creational/ServiceLocator/Scripts/ServicesInitializer.cs
+++ Assets/Creational/ServiceLocator/Scripts/ServicesInitializer.cs
@@ -2,12 +2,23 @@
 
 namespace ServiceLocatorExample
 {
+    /// <summary>
+    ///     Passes all the necessary services.
+    /// </summary>
     public class ServicesInitializer : MonoBehaviour
     {
         private void Awake()
         {
-            //First thing to do
-            ServiceLocator.InitializeServices();
+            var logger = new ServiceLogger();
+            var audioPlayer = new ServiceAudio();
+            var achievements = new ServiceAchievements();
+            
+            //Note: the binding happens with the interface.
+            ServiceLocator.RegisterService<ILogger>(logger);
+            ServiceLocator.RegisterService<IAudioPlayer>(audioPlayer);
+            ServiceLocator.RegisterService<IAchievements>(achievements);
+            
+            logger.Log("Services", "Initialized successfully");
         }
     }
 }
